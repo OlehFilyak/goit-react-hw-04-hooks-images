0937bd738fec36bbd901cd0b784ae46d23f3ec2b
Олег Філяк <73895897+OlehFilyak@users.noreply.github.com>
2021-07-21 13:21:22 +0300

    added markups

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-// import ImageGallary from "./Components/ImageGallary";
+import { useState } from "react";
+
+import ImageGallery from "./Components/ImageGallery";
 // import ImageGalleryItem from "./Components/ImageGalleryItem";
 // import Loader from "./Components/Loader";
 // import Modal from "./Components/Modal";
-// import Button from "./Components/Button";
-// import Searchbar from "./Components/Searchbar";
+import Button from "./Components/Button";
+import Searchbar from "./Components/Searchbar";
 
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [searchQuery, SetSearchQuery] = useState("");
+
+  return (
+    <div className="App">
+      <Searchbar />
+      <ImageGallery />
+      <Button />
+      {/* <Modal/> */}
+    </div>
+  );
 }
 
 export default App;
--- src/Components/ImageGalleryItem/index.js
+++ src/Components/ImageGalleryItem/index.js
@@ -1,1 +1,1 @@
-export { default } from "./ImageGallaryItem";
+export { default } from "./ImageGalleryItem";
