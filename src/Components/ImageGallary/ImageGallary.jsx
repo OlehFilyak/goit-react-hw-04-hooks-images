5b7aafea69f5363afeab0302a97f80e3b92fc5bc
Олег Філяк <73895897+OlehFilyak@users.noreply.github.com>
2021-07-21 12:34:56 +0300

    added structure of project

---
+++ src/Components/ImageGallary/ImageGallary.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ImageGallary() {
+  return <div></div>;
+}
+
+export default ImageGallary;
---
+++ src/Components/ImageGalleryItem/ImageGallaryItem.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ImageGallaryItem() {
+  return <div></div>;
+}
+
+export default ImageGallaryItem;
