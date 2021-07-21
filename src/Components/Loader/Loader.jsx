d6d8d0ba477927007907497db964b490a9c345a0
Олег Філяк <73895897+OlehFilyak@users.noreply.github.com>
2021-07-21 13:20:58 +0300

    added react-loader-spinner

--- src/Components/Loader/Loader.jsx
+++ src/Components/Loader/Loader.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import Loader from "react-loader-spinner";
+const Spinner = () => {
+  //other logic
+  return (
+    <Loader
+      type="Puff"
+      color="#00BFFF"
+      height={100}
+      width={100}
+      timeout={3000} //3 secs
+    />
+  );
+};
 
-function Loader() {
-  return <div></div>;
-}
-
-export default Loader;
+export default Spinner;
